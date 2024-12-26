35668c9de3b706a9df782c551bef95c9d9fd3e24
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-12-26 12:00:00 +1100

    Getting started with the type script as well

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
    <main className="">
 <h1>Getting started with nextjs course </h1>
+<p>How about learning the type script please </p>
    </main>
   );
 }
