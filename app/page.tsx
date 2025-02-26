613bdf5b406131c344d00a1c5a7a98b8c8b9d1f5
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2025-02-26 21:23:10 +1100

    getting hands dirty in next js course and type script

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
    <main className="">
 <h1>Getting started with nextjs course </h1>
 <p>How about learning the type script please </p>
+<p>Thakur Neupane</p>
    </main>
   );
 }
